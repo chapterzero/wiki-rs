10204bffa885159d9058952dba8830ca586bd855
Yusuf Irwandi <yusuf.irwandi@bytedance.com>
2019-10-08 15:59:20 +0700

    [master] Build alpha

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use wikipedia::Wikipedia;
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let page_name: &String = args.get(1)
+        .expect("Require 2nd argument: page_name, Ex: Joko Widodo");
+
     let w = Wikipedia::new("id");
-    let page = w.get_page("titles", "Joko Widodo");
+    let page = w.get_page("titles", page_name.as_ref());
     println!("{:?}", page);
-    // let pages = w.get_cat_members("Kategori:Politikus_Indonesia").unwrap();
-    // for page in &pages {
-    //     println!("{} {:?}", page.title, page.canonicalurl);
-    // }
 }
